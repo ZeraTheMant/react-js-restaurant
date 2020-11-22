a51e2483312db22171ba1de0a41b5dccf1aca08f
Reuben <imbapalagz@gmail.com>
2020-11-22 16:16:55 +0800

    routes

--- src/Routes.js
+++ src/Routes.js
@@ -4,8 +4,9 @@ import App from "./App";
 import Shop from "./Shop";
 
 const Routes = () => {//{process.env.PUBLIC_URL}
+    alert(process.env.PUBLIC_URL)
     return (
-        <BrowserRouter basename="/react-js-restaurant/">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route exact path="/shop" component={Shop} />
