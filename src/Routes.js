f4ef60d5697c026a780acf1cf866511ac5d42164
Reuben <imbapalagz@gmail.com>
2020-11-22 17:18:52 +0800

    routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Shop from "./Shop";
 
 const Routes = () => {//{process.env.PUBLIC_URL}
-    alert(process.env.PUBLIC_URL)
+
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route exact path={process.env.PUBLIC_URL + "/shop"} component={Shop} />
-            </Switch>
-        </BrowserRouter>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+            <Route render = {({ location }) => (
+                <Switch location = { location }>
+                    <Route exact path="/" component={App} />
+                    <Route exact path="/shop" component={Shop} />
+                 </Switch>
+            )} />
+        </HashRouter>
     )
 }
 
